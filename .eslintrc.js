5904c23ef8fe79ebae05db0c702e9b989807a54b
Chanisha <csachdeva83@gmail.com>
2025-06-24 01:28:53 +0530

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
-export default {
+// .eslintrc.js
+module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "google", "prettier"],
+  extends: ["plugin:react/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      tsx: true,
     },
     ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
   settings: {
     react: {
-      version: "latest",
+      version: "detect",
     },
   },
 };
